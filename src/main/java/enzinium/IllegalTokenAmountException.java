26112933d1b07ff48109ea16df4808f70735a8aa
AmarinoLama <amarinolama@gmail.com>
2024-04-21 21:20:18 +0200

    fix (IllegalTokenAmountException) : change type field of firstValue

--- src/main/java/enzinium/IllegalTokenAmountException.java
+++ src/main/java/enzinium/IllegalTokenAmountException.java
@@ -3,20 +3,20 @@ package enzinium;
 import java.security.PublicKey;
 
 public class IllegalTokenAmountException extends IllegalArgumentException {
-    private PublicKey firstValue;
-    private Double secondValue;
+    private double firstValue;
+    private double secondValue;
 
-    public IllegalTokenAmountException(String s, PublicKey firstValue, Double secondValue) {
+    public IllegalTokenAmountException(String s, double firstValue, double secondValue) {
         super(s);
         this.firstValue = firstValue;
         this.secondValue = secondValue;
     }
 
-    public PublicKey getFirstValue() {
+    public double getFirstValue() {
         return firstValue;
     }
 
-    public Double getSecondValue() {
+    public double getSecondValue() {
         return secondValue;
     }
 }
