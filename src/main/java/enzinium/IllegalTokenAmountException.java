f5ff488bbf2a52436c653b2ffb53dee5fb92712d
AmarinoLama <amarinolama@gmail.com>
2024-04-21 21:06:08 +0200

    feat (IllegalTokenAmountException) : add attributes and constructor

--- src/main/java/enzinium/IllegalTokenAmountException.java
+++ src/main/java/enzinium/IllegalTokenAmountException.java
@@ -3,5 +3,12 @@ package enzinium;
 import java.security.PublicKey;
 
 public class IllegalTokenAmountException extends IllegalArgumentException {
+    private PublicKey firstValue;
+    private Double secondValue;
 
+    public IllegalTokenAmountException(String s, PublicKey firstValue, Double secondValue) {
+        super(s);
+        this.firstValue = firstValue;
+        this.secondValue = secondValue;
+    }
 }
